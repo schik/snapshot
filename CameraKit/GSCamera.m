7dbb851d98ce2da41e9da20d1adaf326f530f292
Andreas Schik <andreas@schik.de>
2015-03-11 14:11:17 +0100

    Add private version of CameraKit with patches needed for Snapshot.

---
+++ CameraKit/GSCamera.m
@@ -0,0 +1,165 @@
+#include "GSCamera.h"
+
+@implementation GSCamera
+
+- (id) initWithName: (NSString *) aName
+	       Port: (NSString *) aPort
+	     Camera: (Camera *) aCamera
+{
+  [super init];
+
+  theName = [[NSString alloc] initWithString: aName];
+  thePort = [[NSString alloc] initWithString: aPort];
+  theCamera = aCamera;
+
+  return self;
+}
+
+
+- (void) dealloc
+{
+  RELEASE(theName);
+  RELEASE(thePort);
+  if (theCamera != NULL) {
+    gp_camera_exit(theCamera, NULL);
+    free(theCamera);
+  }
+  [super dealloc];
+}
+
+
+- (NSString *) name
+{
+  return theName;
+}
+
+
+- (NSString *) port
+{
+  return thePort;
+}
+
+
+- (NSArray *) filesInPath: (NSString *) aPath
+{
+  CameraList *list;
+  GPContext *context;
+  int i, count;
+  NSMutableArray *ar = [[NSMutableArray alloc] init];
+
+  context = gp_context_new();
+  gp_list_new(&list);
+  gp_camera_folder_list_files(theCamera, [aPath cString], list, context);
+  
+  count = gp_list_count (list);
+  
+  for (i=0; i<count; i++)
+    {
+      const char *cname;
+      NSString *name;
+      gp_list_get_name  (list, i, &cname);
+      name = [[NSString alloc] initWithCString: cname];
+      [ar addObject: name];
+      RELEASE(name);
+    }
+
+  gp_list_free(list);
+  free(context);
+
+  return ar;
+}
+
+
+- (NSArray *) foldersInPath: (NSString *) aPath
+{
+  CameraList *list;
+  GPContext *context;
+  int i, count;
+  NSMutableArray *ar = [[NSMutableArray alloc] init];
+
+  context = gp_context_new();
+  gp_list_new(&list);
+  gp_camera_folder_list_folders(theCamera, [aPath cString], list, context);
+  
+  count = gp_list_count (list);
+  for (i=0; i<count; i++)
+    {
+      const char *cname;
+      NSString *name;
+      gp_list_get_name  (list, i, &cname);
+      name = [[NSString alloc] initWithCString: cname];
+      [ar addObject: name];
+      RELEASE(name);
+    }
+
+  gp_list_free(list);
+  free(context);
+
+  return ar;
+}
+
+- (void) getFile: (NSString *)aFile from: (NSString *)srcPath to: (NSString *)destPath
+{
+  CameraFile *file;
+  GPContext *context;
+  NSAutoreleasePool *pool = [NSAutoreleasePool new];
+
+  context = gp_context_new();
+  gp_file_new(&file);
+
+  gp_camera_file_get(theCamera, [srcPath cString], [aFile cString],
+		     GP_FILE_TYPE_NORMAL, file, context);
+  gp_file_save(file, [[NSString stringWithFormat: @"%@/%@", destPath, aFile] cString]);
+
+  gp_file_unref(file);
+
+  RELEASE(pool);
+}
+
+
+- (void) deleteFile: (NSString *)file from: (NSString *)path
+{
+  GPContext *context;
+  NSAutoreleasePool *pool = [NSAutoreleasePool new];
+
+  context = gp_context_new();
+
+  gp_camera_file_delete(theCamera, [path cString], [file cString], context);
+
+  RELEASE(pool);
+}
+
+
+- (void) putFile: (NSString *)file from: (NSString *)srcPath to: (NSString *)destPath
+{
+
+}
+
+
+- (NSString *) description
+{
+  return [self name];
+}
+
+- (NSImage *) thumbnailForFile: (NSString *)file inPath: (NSString *)path
+{
+  NSImage *image = nil;
+  CameraFile *cfile;
+  gp_file_new(&cfile);
+  int result = gp_camera_file_get (theCamera, [path cString],
+                  [file cString], GP_FILE_TYPE_PREVIEW, cfile, NULL);
+  if (result >= 0) {
+      const char *fd;
+      unsigned long fs;
+
+      gp_file_get_data_and_size (cfile, &fd, &fs);
+
+      NSData *data = [NSData dataWithBytes: fd length: fs];
+      image = [[NSImage alloc] initWithData: data];
+  } 
+	
+  gp_file_unref (cfile);
+  return image;
+}
+
+@end
---
+++ CameraKit/GSGPhoto2.m
@@ -0,0 +1,128 @@
+#include "GSGPhoto2.h"
+
+#include <gphoto2.h>
+
+@implementation GSGPhoto2
+
+- (id) init
+{
+  [super init];
+
+  [self reinit];
+
+  return self;
+}
+
+
+- (void) reinit
+{
+  NSAutoreleasePool *pool;
+
+  GPContext *context;
+  CameraList *list;
+  CameraAbilitiesList *al = NULL;
+  GPPortInfoList *il = NULL;
+  int count, i;
+
+  pool = [NSAutoreleasePool new];
+
+  context = gp_context_new();
+  gp_list_new (&list);
+  gp_abilities_list_new (&al);
+  gp_abilities_list_load (al, context);
+  gp_port_info_list_new (&il);
+  gp_port_info_list_load (il);
+
+  gp_abilities_list_detect (al, il, list, context);
+
+  gp_abilities_list_free (al);
+  gp_port_info_list_free (il);
+
+  count = gp_list_count (list);
+
+  theCameraList = [[NSMutableArray alloc] init];
+
+  for (i = 0; i < count; i++)
+    {
+      Camera *camera;
+      char *name;
+      char *value;
+      CameraAbilitiesList *abilities_list = NULL;
+      CameraAbilities a;
+      GPPortInfo info;
+      GSCamera *cam;
+
+      int m, p;
+
+      gp_list_get_name  (list, i, &name);
+      gp_list_get_value (list, i, &value);
+
+      gp_camera_new(&camera);
+
+      gp_abilities_list_new (&abilities_list);
+      gp_abilities_list_load (abilities_list, context);
+      gp_port_info_list_new (&il);
+      gp_port_info_list_load (il);
+
+      m = gp_abilities_list_lookup_model(abilities_list, name);
+      gp_abilities_list_get_abilities (abilities_list, m, &a);
+      gp_camera_set_abilities (camera, a);
+
+      p = gp_port_info_list_lookup_path (il, value);
+      gp_port_info_list_get_info (il, p, &info);
+      gp_camera_set_port_info (camera, info);
+
+      gp_setting_set ("campics", "model", name);
+      gp_setting_set ("campics", "port", value);
+
+      gp_port_info_list_free(il);
+
+      cam = [[GSCamera alloc] initWithName: [NSString stringWithCString: name]
+			      Port: [NSString stringWithCString: value]
+			      Camera: camera];
+
+      [theCameraList addObject: cam];
+            
+      RELEASE(cam);
+
+//      free(name);
+//      free(value);
+    }
+
+  RELEASE(pool);
+}
+
+
+- (void) dealloc
+{
+  RELEASE(theCameraList);
+  [super dealloc];
+}
+
+
+- (unsigned) numberOfCameras
+{
+  return [theCameraList count];
+}
+
+
+- (NSString *) nameOfCameraAtIndex: (int) anIndex
+{
+  GSCamera *cam;
+  cam = [theCameraList objectAtIndex: anIndex];
+  return [cam name];
+}
+
+
+- (int) indexOfCameraNamed: (NSString *) aName
+{
+  return -1;
+}
+
+
+- (GSCamera *) cameraAtIndex: (int) anIndex
+{
+  return [theCameraList objectAtIndex: anIndex];
+}
+
+@end
