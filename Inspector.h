58b1fe0a67f5bcf9574361cb4ca9047e2164cf1b
schik <andreas@schik.de>
2015-03-16 22:24:48 +0100

    Add Inspector main window

---
+++ Inspector.h
@@ -0,0 +1,66 @@
+/* Inspector.h
+ *  
+ * Copyright (C) 2015
+ *
+ * Author: Andreas Schik <andreas@schik.de>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02111 USA.
+ */
+
+#ifndef INSPECTOR_H
+#define INSPECTOR_H
+
+#include <Foundation/Foundation.h>
+
+@class Attributes;
+@class NSPopUpButton;
+@class NSWindow;
+
+@interface Inspector : NSObject 
+{
+    NSWindow *win;
+    NSPopUpButton *popUp;
+    NSBox *inspBox;
+
+    NSMutableArray *inspectors;
+    id currentInspector;
+
+    NSNotificationCenter *nc; 
+}
+
+- (void)activate;
+
+- (void)setCurrentSelection:(NSArray *)selection;
+
+- (BOOL)canDisplayDataOfType:(NSString *)type;
+
+- (void)showData:(NSData *)data 
+          ofType:(NSString *)type;
+
+- (IBAction)activateInspector:(id)sender;
+
+- (void)showAttributes;
+
+- (id)attributes;
+
+- (NSWindow *)win;
+
+- (void)updateDefaults;
+
+- (void)watcherNotification:(NSNotification *)notif;
+
+@end
+
+#endif // INSPECTOR_H
